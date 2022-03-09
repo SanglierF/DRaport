083eadf05c200521c2d59e702855ce0afb4eb13a
SanglierF <lightrevan1996@gmail.com>
2022-03-09 15:01:50 +0100

    ## Added: -Workdays to drawer navigator ## Changed: -App.tsx formatting

--- App.tsx
+++ App.tsx
@@ -5,64 +5,66 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 import HomeScreen from "./screens/HomeScreen";
 import Clients from "./screens/clients/Clients";
 import Products from "./screens/products/Products";
 import Warehouses from "./screens/warehouses/Warehouses";
+import Workdays from "./screens/workdays/Workdays";
 import DbContext from "./DbContext";
 import LocalDatabase from "./database/LocalDatabase";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-
   const queryClient = new QueryClient();
 
   const localDatabase = new LocalDatabase();
 
   return (
     <DbContext.Provider value={localDatabase}>
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName="Home"
-          screenOptions={{ headerTitleAlign: "center" }}
-        >
-          <Drawer.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ title: "Main screen" }}
-          />
-          <Drawer.Screen
-            name="Clients"
-            component={Clients}
-            options={{
-              title: "Clients",
-              headerShown: false,
-              unmountOnBlur: true
-            }}
-          />
-          <Drawer.Screen
-            name="Products"
-            component={Products}
-            options={{
-              title: "Products",
-              headerShown: false,
-              unmountOnBlur: true
-            }}
-          />
-          <Drawer.Screen
-            name="Warehouses"
-            component={Warehouses}
-            options={{
-              title: "Warehouses",
-              headerShown: false,
-              unmountOnBlur: true
-            }}
-          />
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home" screenOptions={{ headerTitleAlign: "center" }}>
+            <Drawer.Screen name="Home" component={HomeScreen} options={{ title: "Main screen" }} />
+            <Drawer.Screen
+              name="Clients"
+              component={Clients}
+              options={{
+                title: "Clients",
+                headerShown: false,
+                unmountOnBlur: true
+              }}
+            />
+            <Drawer.Screen
+              name="Products"
+              component={Products}
+              options={{
+                title: "Products",
+                headerShown: false,
+                unmountOnBlur: true
+              }}
+            />
+            <Drawer.Screen
+              name="Warehouses"
+              component={Warehouses}
+              options={{
+                title: "Warehouses",
+                headerShown: false,
+                unmountOnBlur: true
+              }}
+            />
+            <Drawer.Screen
+              name="Workdays"
+              component={Workdays}
+              options={{
+                title: "Workdays",
+                headerShown: false,
+                unmountOnBlur: true
+              }}
+            />
+          </Drawer.Navigator>
+        </NavigationContainer>
       </QueryClientProvider>
     </DbContext.Provider>
   );
