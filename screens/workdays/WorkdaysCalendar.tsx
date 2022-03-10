5613ba37a42fe27f1a975ab25df1811f944a4abf
SanglierF <lightrevan1996@gmail.com>
2022-03-10 13:00:04 +0100

    ## Added: -Calendar to calendar screen

--- screens/workdays/WorkdaysCalendar.tsx
+++ screens/workdays/WorkdaysCalendar.tsx
@@ -8,5 +8,43 @@ import ModalConfirmation from "../../components/ModalConfirmation";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 export default function WorkdaysCalendar() {
-  return <View></View>;
+  return (
+    <View>
+      <Calendar
+        // Handler which gets executed on day press. Default = undefined
+        onDayPress={(day) => {
+          let d = new Date(day.timestamp)
+          console.log("selected day", d.toISOString());
+        }}
+        onDayLongPress={(day) => {
+          const td = new Date();
+          console.log("today is", td);
+        }}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={"yyyy MM"}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={(month) => {
+          console.log("month changed", month);
+        }}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={false}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={true}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={(subtractMonth) => subtractMonth()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={(addMonth) => addMonth()}
+        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+        disableAllTouchEventsForDisabledDays={true}
+        // Replace default month and year title with custom one. the function receive a date as parameter
+        //renderHeader={(date) => {
+        /*Return JSX*/
+        //}}
+        // Enable the option to swipe between months. Default = false
+        enableSwipeMonths={true}
+      />
+    </View>
+  );
 }
