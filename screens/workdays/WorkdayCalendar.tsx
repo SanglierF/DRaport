945b432c4b4bfa2e0b93d1d3964d89f5102cd8ac
SanglierF <lightrevan1996@gmail.com>
2022-03-11 12:07:45 +0100

    ## Added: -markeddays template to calendar

--- screens/workdays/WorkdayCalendar.tsx
+++ screens/workdays/WorkdayCalendar.tsx
@@ -2,28 +2,66 @@ import * as React from "react";
 import { StyleSheet, Text, View, FlatList, Pressable, Image } from "react-native";
 import { List, Button, FAB, Divider, Modal } from "react-native-paper";
 import { useIsFocused, useFocusEffect } from "@react-navigation/native";
-import DbContext from "../../DbContext";
-import ClientRepository from "../../database/repositories/ClientRepository";
+import LocalDatabase from "../../database/LocalDatabase";
+import WorkdayRepository from "../../database/repositories/WorkdayRepository";
 import ModalConfirmation from "../../components/ModalConfirmation";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
-export default function WorkdayCalendar() {
+export default function WorkdayCalendar({ navigation }) {
+  const localDb = LocalDatabase.getInstance();
+  const workdayRepository = new WorkdayRepository(localDb.dbConnection);
+
+  const [markedDates, setMarkedDates] = React.useState<{
+    [key: string]: {
+      marked?: boolean;
+      selected?: boolean;
+      selectedColor?: string;
+      dotColor?: string;
+      disabled?: boolean;
+      activeOpacity?: number;
+      disableTouchEvent?: boolean;
+    };
+  }>({
+    "2022-03-02": { selected: true, marked: true, selectedColor: "blue" },
+    "2022-03-07": { marked: true },
+    "2022-03-13": { marked: true, dotColor: "red", activeOpacity: 0 },
+    "2022-03-11": { disabled: true, disableTouchEvent: true },
+  });
+
+  function gotoDay(day: Date) {
+    console.log("selected day", day.toISOString());
+    // TODO find day
+    // ifdoesntexist dont do anything else
+    // navigation.navigate("Workday", params: {workdayId: 0})
+  }
+
+  function changeMarked() {
+    setMarkedDates({
+      "2022-03-03": { selected: true, marked: true, selectedColor: "blue" },
+      "2022-03-08": { marked: true },
+      "2022-03-22": { marked: true, dotColor: "red", activeOpacity: 0 },
+      "2022-03-11": { disabled: true, disableTouchEvent: true },
+    });
+  }
+  function viewMarked() {
+    console.log(markedDates);
+  }
+
   return (
     <View>
       <Calendar
-        // Handler which gets executed on day press. Default = undefined
         onDayPress={(day) => {
-          let d = new Date(day.timestamp)
-          console.log("selected day", d.toISOString());
+          gotoDay(new Date(day.timestamp));
         }}
         onDayLongPress={(day) => {
-          const td = new Date();
-          console.log("today is", td);
+          // TODO if doesnt exist create new day
+          console.log("longprees");
         }}
         // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
         monthFormat={"yyyy MM"}
         // Handler which gets executed when visible month changes in calendar. Default = undefined
         onMonthChange={(month) => {
+          // TODO rerender calendar with this month
           console.log("month changed", month);
         }}
         // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
@@ -44,7 +82,14 @@ export default function WorkdayCalendar() {
         //}}
         // Enable the option to swipe between months. Default = false
         enableSwipeMonths={true}
+        markedDates={markedDates}
       />
+      <Button onPress={changeMarked} mode="contained">
+        Zmien
+      </Button>
+      <Button onPress={viewMarked} mode="contained">
+        view
+      </Button>
     </View>
   );
 }
