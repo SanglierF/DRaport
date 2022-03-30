219df1649af96ff207633ad7c18bdfd5647437cf
SanglierF <lightrevan1996@gmail.com>
2022-03-30 16:01:21 +0200

    ## Changed: - extracted textinput from clientform

--- screens/clients/ClientForm.tsx
+++ screens/clients/ClientForm.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import axios from "axios";
 import { useQuery } from "react-query";
+import ClientFormInput from "./ClientFormInput";
 import styleForm from "../../styles/styleForm";
+import ClientType from "./ClientType";
 
-export default function ClientForm({clientDetails, setClientDetails, disableFillData=true}) {
+export default function ClientForm({
+  clientDetails,
+  setClientDetails,
+  disableFillData = true,
+}: {
+  clientDetails: ClientType;
+  setClientDetails: any;
+  disableFillData: boolean;
+}) {
   const [loadingStatus, setLoadingStatus] = React.useState(false);
-
-  const handleChange = (key, value) => {
-    setClientDetails({ ...clientDetails, [key]: value });
-  };
 
   function fillData() {
     setLoadingStatus(true);
@@ -62,87 +68,16 @@ export default function ClientForm({clientDetails, setClientDetails, disableFill
   });
 
   return (
-    <View style={styleForm.containerForm}>
+    <ScrollView style={{flex: 1}}>
       <View style={styleForm.containerInputs}>
         {loadingStatus ? <Text>'Loading data from GUS...'</Text> : null}
-        <TextInput
-          style={styleForm.textInput}
-          label="Client name"
-          mode="outlined"
-          value={clientDetails.name}
-          onChangeText={(newName) => handleChange("name", newName)}
-          autoComplete="off"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="nickname"
-          mode="outlined"
-          value={clientDetails.nickname}
-          onChangeText={(newNickname) => handleChange("nickname", newNickname)}
-          autoComplete="off"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client nip"
-          mode="outlined"
-          value={clientDetails.nip}
-          onChangeText={(newNip) => handleChange("nip", newNip)}
-          autoComplete="off"
-          keyboardType="decimal-pad"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client regon"
-          mode="outlined"
-          value={clientDetails.regon}
-          onChangeText={(newRegon) => handleChange("regon", newRegon)}
-          autoComplete="off"
-          keyboardType="decimal-pad"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client voivodeship"
-          mode="outlined"
-          value={clientDetails.voivodeship}
-          onChangeText={(newVoivodeship) => handleChange("voivodeship", newVoivodeship)}
-          autoComplete="off"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client city"
-          mode="outlined"
-          value={clientDetails.city}
-          onChangeText={(newCity) => handleChange("city", newCity)}
-          autoComplete="off"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client zip"
-          mode="outlined"
-          value={clientDetails.zip}
-          onChangeText={(newZip) => handleChange("zip", newZip)}
-          autoComplete="off"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client street"
-          mode="outlined"
-          value={clientDetails.street}
-          onChangeText={(newStreet) => handleChange("street", newStreet)}
-          autoComplete="off"
-        />
-        <TextInput
-          style={styleForm.textInput}
-          label="Client tel number"
-          mode="outlined"
-          value={clientDetails.tel}
-          onChangeText={(newTel) => handleChange("tel", newTel)}
-          autoComplete="off"
-        />
+        <ClientFormInput clientDetails={clientDetails} setClientDetails={setClientDetails} />
       </View>
-      { disableFillData? null: <Button style={styleForm.buttonAdd} onPress={fillData} mode="contained">
-        Fill data
-      </Button>}
-    </View>
+      {disableFillData ? null : (
+        <Button style={styleForm.buttonAdd} onPress={fillData} mode="contained">
+          Fill data
+        </Button>
+      )}
+    </ScrollView>
   );
 }
