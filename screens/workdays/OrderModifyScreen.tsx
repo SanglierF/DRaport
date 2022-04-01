b18bf2037f7f91b04f8650b4e59fc351d9b6c622
SanglierF <lightrevan1996@gmail.com>
2022-04-01 22:25:44 +0200

    ## Changed: -Added dependencies to dependency arrays -Used useRef hook to initialize repositories once

--- screens/workdays/OrderModifyScreen.tsx
+++ screens/workdays/OrderModifyScreen.tsx
@@ -1,34 +1,35 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
 import { Button, FAB } from "react-native-paper";
-import { useIsFocused } from "@react-navigation/native";
 import LocalDatabase from "../../database/LocalDatabase";
 import OrderRepository from "../../database/repositories/OrderRepository";
 import OrderedProductRepository from "../../database/repositories/OrderedProductRepository";
 import ProductRepository from "../../database/repositories/ProductRepository";
 import WarehouseRepository from "../../database/repositories/WarehouseRepository";
+import { Warehouse } from "../../database/entities/Warehouse";
+import { OrderedProduct } from "../../database/entities/OrderedProduct";
+import { Order } from "../../database/entities/Order";
 import { ContextOrderProductList } from "./Workdays";
 import OrderComponent from "./OrderComponent";
 
-export default function OrderAddScreen({ navigation, route }) {
-  const localDb = LocalDatabase.getInstance();
-  const orderRepository = new OrderRepository(localDb.dbConnection);
-  const orderedProductRepository = new OrderedProductRepository(localDb.dbConnection);
-  const productRepository = new ProductRepository(localDb.dbConnection);
-  const warehouseRepository = new WarehouseRepository(localDb.dbConnection);
+export default function OrderModifyScreen({ navigation, route }) {
+  const dbConnection = React.useRef(LocalDatabase.getInstance().dbConnection);
 
-  const [warehouseList, setWarehouseList] = React.useState([]);
+  const orderRepository = React.useRef(new OrderRepository(dbConnection.current));
+  const orderedProductRepository = React.useRef(new OrderedProductRepository(dbConnection.current));
+  const productRepository = React.useRef(new ProductRepository(dbConnection.current));
+  const warehouseRepository = React.useRef(new WarehouseRepository(dbConnection.current));
+
+  const [warehouseList, setWarehouseList] = React.useState<Warehouse[]>([]);
   const [warehouseId, setWarehouseId] = React.useState(-1);
-  const [order, setOrder] = React.useState(null);
-  const [orderedProducts, setOrderedProducts] = React.useState([]);
-  const [changeCounter, setChangeCounter] = React.useState(0);
+  const [order, setOrder] = React.useState<Order>(null);
+  const [orderedProducts, setOrderedProducts] = React.useState<OrderedProduct[]>([]);
+  const [removedOrderedProductsIds, setRemovedOrderedProductsIds] = React.useState<number[]>([]);
 
-  const contextOrderProductList = React.useContext(ContextOrderProductList);
-
-  let isFocused = useIsFocused();
+  const contextOrderProductList = React.useRef(React.useContext(ContextOrderProductList));
 
   React.useEffect(() => {
-    orderRepository
+    orderRepository.current
       .findById(route.params.orderId)
       .then((order) => {
         setOrder(order);
@@ -37,10 +38,12 @@ export default function OrderAddScreen({ navigation, route }) {
       .catch((e) => {
         console.log(e);
       });
-    warehouseRepository
+  }, [route.params.orderId]);
+  React.useEffect(() => {
+    warehouseRepository.current
       .getAll()
       .then((warehouse) => {
-        const noWarehouse = warehouseRepository.create({ nickname: "Default" });
+        const noWarehouse = warehouseRepository.current.create({ nickname: "Default" });
         noWarehouse.id = -1;
         const warehouses = [noWarehouse].concat(warehouse);
         setWarehouseList(warehouses);
@@ -52,12 +55,12 @@ export default function OrderAddScreen({ navigation, route }) {
   }, []);
 
   React.useEffect(() => {
-    orderedProductRepository
+    orderedProductRepository.current
       .getAllInOrder(order)
       .then((orderedProducts) => {
         setOrderedProducts(orderedProducts);
         const selectedProducts = orderedProducts.map((orderedProduct) => orderedProduct.product.id);
-        contextOrderProductList.setOrderedProducts(selectedProducts);
+        contextOrderProductList.current.setOrderedProducts(selectedProducts);
         return orderedProducts;
       })
       .catch((e) => {
@@ -67,12 +70,11 @@ export default function OrderAddScreen({ navigation, route }) {
 
   React.useEffect(() => {
     if (route.params?.productId) {
-      productRepository
+      productRepository.current
         .findById(route.params.productId)
         .then((product) => {
-          const orderedProduct = orderedProductRepository.create(order, product, 1);
-          setOrderedProducts(orderedProducts.concat(orderedProduct));
-          setChangeCounter((changeCounter) => changeCounter++);
+          const orderedProduct = orderedProductRepository.current.create(order, product, 1);
+          setOrderedProducts((orderedProducts) => orderedProducts.concat(orderedProduct));
           return product;
         })
         .catch((e) => {
@@ -80,33 +82,44 @@ export default function OrderAddScreen({ navigation, route }) {
         });
       navigation.setParams({ productId: undefined });
     }
-  }, [isFocused]);
+  }, [navigation, order, route.params?.productId]);
 
   React.useEffect(() => {
     const selectedProducts = orderedProducts.map((orderedProduct) => orderedProduct.product.id);
-    contextOrderProductList.setOrderedProducts(selectedProducts);
-  }, [changeCounter]);
+    contextOrderProductList.current.setOrderedProducts(selectedProducts);
+  }, [orderedProducts]);
 
-  function deleteOrderedProduct(productId) {
+  function deleteOrderedProduct(productId: number) {
+    orderedProducts.forEach((orderedProduct) => {
+      if (orderedProduct.product.id === productId && orderedProduct.id) {
+        setRemovedOrderedProductsIds((removedOrderedProductsIds) =>
+          removedOrderedProductsIds.concat(orderedProduct.id)
+        );
+      }
+    });
     setOrderedProducts(
       orderedProducts.filter((orderedProduct) => {
         return !(orderedProduct.product.id === productId);
       })
     );
-    setChangeCounter((counter) => counter++);
   }
 
   function saveOrder() {
-    orderRepository
-      .save(order)
-      .then((order) => {
-        orderedProducts.forEach((orderedProduct) => (orderedProduct.order = order));
-        orderedProductRepository.saveAll(orderedProducts);
-        return order;
-      })
-      .catch((e) => {
-        console.log(e);
+    (async () => {
+      removedOrderedProductsIds.forEach((removedOrderedProductId) => {
+        orderedProductRepository.current.delete(removedOrderedProductId);
       });
+      orderRepository.current
+        .save(order)
+        .then((order) => {
+          orderedProducts.forEach((orderedProduct) => (orderedProduct.order = order));
+          orderedProductRepository.current.saveAll(orderedProducts);
+          return order;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    })();
     navigation.goBack();
   }
   return (
