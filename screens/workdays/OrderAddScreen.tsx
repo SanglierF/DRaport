36634fecd6173bb2c9c926856906d297e218f437
SanglierF <lightrevan1996@gmail.com>
2022-04-01 20:46:37 +0200

    ## Changed: -removed unusued imports -added dependencies to arrays -used useRef so there wont be reinitialization of repositories

--- screens/workdays/OrderAddScreen.tsx
+++ screens/workdays/OrderAddScreen.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
 import { Button, FAB } from "react-native-paper";
-import { useIsFocused } from "@react-navigation/native";
 import LocalDatabase from "../../database/LocalDatabase";
 import VisitRepository from "../../database/repositories/VisitRepository";
 import OrderRepository from "../../database/repositories/OrderRepository";
@@ -9,77 +8,82 @@ import OrderedProductRepository from "../../database/repositories/OrderedProduct
 import ProductRepository from "../../database/repositories/ProductRepository";
 import WarehouseRepository from "../../database/repositories/WarehouseRepository";
 import { OrderedProduct } from "../../database/entities/OrderedProduct";
+import { Visit } from "../../database/entities/Visit";
+import { Order } from "../../database/entities/Order";
+import { Warehouse } from "../../database/entities/Warehouse";
 import { ContextOrderProductList } from "./Workdays";
 import OrderComponent from "./OrderComponent";
 
 export default function OrderAddScreen({ navigation, route }) {
-  const localDb = LocalDatabase.getInstance();
-  const visitRepository = new VisitRepository(localDb.dbConnection);
-  const orderRepository = new OrderRepository(localDb.dbConnection);
-  const orderedProductRepository = new OrderedProductRepository(localDb.dbConnection);
-  const productRepository = new ProductRepository(localDb.dbConnection);
-  const warehouseRepository = new WarehouseRepository(localDb.dbConnection);
+  const dbConnection = React.useRef(LocalDatabase.getInstance().dbConnection);
 
-  const [visit, setVisit] = React.useState(null);
-  const [warehouseList, setWarehouseList] = React.useState([]);
+  const visitRepository = React.useRef(new VisitRepository(dbConnection.current));
+  const warehouseRepository = React.useRef(new WarehouseRepository(dbConnection.current));
+  const orderRepository = React.useRef(new OrderRepository(dbConnection.current));
+  const orderedProductRepository = React.useRef(new OrderedProductRepository(dbConnection.current));
+  const productRepository = React.useRef(new ProductRepository(dbConnection.current));
+
+  const [visit, setVisit] = React.useState<Visit>(null);
+  const [warehouseList, setWarehouseList] = React.useState<Warehouse[]>([]);
   const [warehouseId, setWarehouseId] = React.useState(-1);
-  const [order, setOrder] = React.useState(null);
+  const [order, setOrder] = React.useState<Order>(null);
   const [orderedProducts, setOrderedProducts] = React.useState<OrderedProduct[]>([]);
-  const [changeCounter, setChangeCounter] = React.useState(0);
 
-  const contextOrderProductList = React.useContext(ContextOrderProductList);
-
-  let isFocused = useIsFocused();
+  const contextOrderProductList = React.useRef(React.useContext(ContextOrderProductList));
 
   React.useEffect(() => {
-    visitRepository
-      .findById(route.params.visitId)
-      .then((visit) => {
+    async function fetchVisit() {
+      try {
+        const visit = await visitRepository.current.findById(route.params.visitId);
         setVisit(visit);
-        return visit;
-      })
-      .catch((e) => {
+      } catch (e) {
         console.log(e);
-      });
-    warehouseRepository
-      .getAll()
-      .then((warehouse) => {
-        const noWarehouse = warehouseRepository.create({ nickname: "Default" });
+      }
+    }
+    fetchVisit();
+  }, [route.params.visitId]);
+
+  React.useEffect(() => {
+    async function fetchWarehouses() {
+      try {
+        const warehouses = await warehouseRepository.current.getAll();
+        const noWarehouse = warehouseRepository.current.create({ nickname: "Default" });
         noWarehouse.id = -1;
-        const warehouses = [noWarehouse].concat(warehouse);
-        setWarehouseList(warehouses);
-        return warehouse;
-      })
-      .catch((e) => {
+        const warehousesList = [noWarehouse].concat(warehouses);
+        setWarehouseList(warehousesList);
+      } catch (e) {
         console.log(e);
-      });
+      }
+    }
+    fetchWarehouses();
   }, []);
 
   React.useEffect(() => {
-    setOrder(orderRepository.create(visit));
+    if (visit !== null) {
+      setOrder(orderRepository.current.create(visit));
+    }
   }, [visit]);
 
   React.useEffect(() => {
+    async function createProduct() {
+      try {
+        const product = await productRepository.current.findById(route.params.productId);
+        const orderedProduct = orderedProductRepository.current.create(order, product, 1);
+        setOrderedProducts((orderedProducts) => orderedProducts.concat(orderedProduct));
+      } catch (e) {
+        console.log(e);
+      }
+    }
     if (route.params?.productId) {
-      productRepository
-        .findById(route.params.productId)
-        .then((product) => {
-          const orderedProduct = orderedProductRepository.create(order, product, 1);
-          setOrderedProducts((orderedProducts) => orderedProducts.concat(orderedProduct));
-          setChangeCounter((changeCounter) => changeCounter++);
-          return product;
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      createProduct();
       navigation.setParams({ productId: undefined });
     }
-  }, [isFocused]);
+  }, [navigation, order, route.params?.productId]);
 
   React.useEffect(() => {
     const selectedProducts = orderedProducts.map((orderedProduct) => orderedProduct.product.id);
-    contextOrderProductList.setOrderedProducts(selectedProducts);
-  }, [changeCounter]);
+    contextOrderProductList.current.setOrderedProducts(selectedProducts);
+  }, [orderedProducts]);
 
   function deleteOrderedProduct(productId) {
     setOrderedProducts(
@@ -87,23 +91,20 @@ export default function OrderAddScreen({ navigation, route }) {
         return !(orderedProduct.product.id === productId);
       })
     );
-    setChangeCounter(changeCounter + 1);
   }
 
   function saveOrder() {
-    orderRepository
-      .save(order)
-      .then((order) => {
-        orderedProducts.forEach((orderedProduct) => (orderedProduct.order = order));
-        orderedProductRepository.saveAll(orderedProducts);
-        return orderedProducts;
-      })
-      .catch((e) => {
+    (async () => {
+      try {
+        const savedOrder = await orderRepository.current.save(order);
+        orderedProducts.forEach((orderedProduct) => (orderedProduct.order = savedOrder));
+        await orderedProductRepository.current.saveAll(orderedProducts);
+      } catch (e) {
         console.log(e);
-      });
+      }
+    })();
     navigation.goBack();
-  } // TODO in flatlist keyExtractor there is no item.id because the orders arent saved untill button press
-  // TODO option to add warehouse to order and send productids in orderedproducts so they aren't shown in product lists pass context with param arrays
+  }
   return (
     <View style={{ flex: 1 }}>
       <OrderComponent
