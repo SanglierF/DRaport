11408efcec88ebb77624dbf08cba75a74f61131f
SanglierF <lightrevan1996@gmail.com>
2022-04-05 15:16:32 +0200

    ## Resolved #19: -extracted reusable code for visit

--- screens/workdays/VisitAddScreen.tsx
+++ screens/workdays/VisitAddScreen.tsx
@@ -1,142 +1,110 @@
 import * as React from "react";
-import { StyleSheet, View, FlatList, Text } from "react-native";
-import { List, Button, FAB, Divider, TextInput } from "react-native-paper";
-import LocalDatabase from "../../database/LocalDatabase";
+import { StyleSheet, View, Text } from "react-native";
+import { Button, FAB } from "react-native-paper";
 import { useIsFocused } from "@react-navigation/native";
+import LocalDatabase from "../../database/LocalDatabase";
 import OrderRepository from "../../database/repositories/OrderRepository";
 import ClientRepository from "../../database/repositories/ClientRepository";
 import WorkdayRepository from "../../database/repositories/WorkdayRepository";
 import VisitRepository from "../../database/repositories/VisitRepository";
-import styleItemDetails from "../../styles/styleItemDetails";
 import ModalConfirmation from "../../components/ModalConfirmation";
-import { ContextVisitedClients } from "./Workdays";
+import OrderlistComponent from "./OrderlistComponent";
 
 export default function VisitAddScreen({ navigation, route }: any) {
-  const localDb = LocalDatabase.getInstance();
-  const orderRepository = new OrderRepository(localDb.dbConnection);
-  const clientRepository = new ClientRepository(localDb.dbConnection);
-  const workdayRepository = new WorkdayRepository(localDb.dbConnection);
-  const visitRepository = new VisitRepository(localDb.dbConnection);
+  const dbConnection = React.useRef(LocalDatabase.getInstance().dbConnection);
+  const orderRepository = React.useRef(new OrderRepository(dbConnection.current));
+  const clientRepository = React.useRef(new ClientRepository(dbConnection.current));
+  const workdayRepository = React.useRef(new WorkdayRepository(dbConnection.current));
+  const visitRepository = React.useRef(new VisitRepository(dbConnection.current));
 
   const [orderList, setOrderList] = React.useState([]);
-  const [changeCounter, setChangeCounter] = React.useState(0);
   const [modalVisible, setModalVisible] = React.useState(false);
   const [deleteOrderId, setDeleteOrderId] = React.useState(-1);
   const [visit, setVisit] = React.useState(null);
   const [client, setClient] = React.useState(null);
   const [workday, setWorkday] = React.useState(null);
   const [addVisitDisabled, setAddVisitDisabled] = React.useState(true);
+  const [changeCounter, setChangeCounter] = React.useState(0);
 
   let isFocused = useIsFocused();
 
-  const contextVisitedClients = React.useContext(ContextVisitedClients);
-
   React.useEffect(() => {
-    workdayRepository.findById(route.params.workdayId).then((found) => {
-      setWorkday(found);
-      if (!client) {
-        navigation.navigate("VisitClient", { previousScreenName: route.name });
+    async function fetchWorkday() {
+      try {
+        const found = await workdayRepository.current.findById(route.params.workdayId);
+        setWorkday(found);
+      } catch (e) {
+        console.log(e);
       }
-    });
-  }, []);
-
-  React.useEffect(() => {
-    if (visit) {
-      orderRepository.getAllInVisit(visit).then((found) => {
-        setOrderList(found);
-      });
     }
-  }, [isFocused, changeCounter, visit]);
+    fetchWorkday();
+  }, [route.params.workdayId]);
 
   React.useEffect(() => {
-    if (route.params?.clientId) {
-      clientRepository.findById(route.params.clientId).then((found) => {
+    if (client === null) {
+      navigation.navigate("VisitClient", { previousScreenName: route.name });
+      return;
+    }
+  }, [client, navigation, route.name]);
+
+  React.useEffect(() => {
+    async function fetchClient() {
+      try {
+        const found = await clientRepository.current.findById(route.params.clientId);
         setClient(found);
-      });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (route.params?.clientId) {
+      fetchClient();
     }
   }, [route.params?.clientId]);
 
   React.useEffect(() => {
-    if (client === null) {
-      return;
-    }
-    if (!visit) {
-      const newVisit = visitRepository.create(workday, client);
-      visitRepository.save(newVisit).then(() => {
+    async function createVisit() {
+      try {
+        const newVisit = visitRepository.current.create(workday, client);
+        await visitRepository.current.save(newVisit);
         //TODO if multiple clients are allowed in a single workday it has to be changed to functionality returning id
-        visitRepository.findVisitByWorkdayClient(workday, client).then((found) => {
-          setVisit(found);
-          setAddVisitDisabled(false);
-        });
-      });
-    } else {
-      visit.client = client;
-      visitRepository.save(visit);
+        const found = await visitRepository.current.findVisitByWorkdayClient(workday, client);
+        setVisit(found);
+        setAddVisitDisabled(false);
+      } catch (e) {
+        console.log(e);
+      }
     }
-  }, [client]);
+    async function updateClient() {
+      try {
+        visit.client = client;
+        visitRepository.current.save(visit);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (!client || !workday) return;
+    if (!visit) {
+      createVisit();
+    } else if (visit.client.id !== client.id) {
+      updateClient();
+    }
+  }, [client, visit, workday]);
 
-  React.useEffect(() => {}, [visit]);
-
-  function renderOrderItem({ item }) {
-    return (
-      <View>
-        <List.Accordion title={item.id} left={(props) => <List.Icon {...props} icon="basket" />}>
-          <FlatList
-            extraData={isFocused}
-            renderItem={renderOrderedProductItem}
-            data={item.orderedProducts}
-            keyExtractor={(item) => item.id}
-            ItemSeparatorComponent={Divider}
-          />
-          <List.Item
-            title={() => (
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-around",
-                }}
-              >
-                <Button
-                  icon="account-edit"
-                  mode="text"
-                  onPress={() => {
-                    navigation.navigate("ModifyOrder", {
-                      orderId: item.id,
-                    });
-                  }}
-                >
-                  Edytuj
-                </Button>
-                <Button
-                  icon="delete"
-                  mode="text"
-                  onPress={() => {
-                    setDeleteOrderId(item.id);
-                    setModalVisible(true);
-                  }}
-                >
-                  Usuń
-                </Button>
-              </View>
-            )}
-            right={() => <View />}
-          />
-        </List.Accordion>
-      </View>
-    );
-  }
-
-  function renderOrderedProductItem({ item }) {
-    return (
-      <List.Item
-        title={`Product: ${item.product.name} Price: ${item.product.price} Number: ${item.quantity}`}
-        right={() => <View />}
-      />
-    );
-  }
+  React.useEffect(() => {
+    async function fetchOrderList() {
+      try {
+        const orders = await orderRepository.current.getAllInVisit(visit);
+        setOrderList(orders);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (!visit) return;
+    fetchOrderList();
+  }, [visit, isFocused, changeCounter]);
 
   function deleteOrder(id: number) {
-    orderRepository.delete(id);
+    orderRepository.current.delete(id);
     setChangeCounter(changeCounter + 1);
   }
 
@@ -147,19 +115,10 @@ export default function VisitAddScreen({ navigation, route }: any) {
   function goAddOrder() {
     navigation.navigate("AddOrder", { visitId: visit.id });
   }
-
-  function Orders() {
-    return (
-      <View>
-        <FlatList
-          extraData={isFocused}
-          renderItem={renderOrderItem}
-          data={orderList}
-          keyExtractor={(order) => order.id}
-          ItemSeparatorComponent={Divider}
-        />
-      </View>
-    );
+  function modifyOrder(id) {
+    navigation.navigate("ModifyOrder", {
+      orderId: id,
+    });
   }
 
   return (
@@ -168,7 +127,17 @@ export default function VisitAddScreen({ navigation, route }: any) {
       <Button onPress={selectClient} mode="contained" style={{ width: 200, alignSelf: "flex-end" }}>
         Wybierz
       </Button>
-      {client ? <Orders /> : <Text>Select client</Text>}
+      {client ? (
+        <OrderlistComponent
+          isFocused={isFocused}
+          orderList={orderList}
+          setDeleteOrderId={setDeleteOrderId}
+          setModalVisible={setModalVisible}
+          modifyOrder={modifyOrder}
+        />
+      ) : (
+        <Text>Select client</Text>
+      )}
       {client ? (
         <FAB
           style={localStyle.fab}
@@ -189,9 +158,6 @@ export default function VisitAddScreen({ navigation, route }: any) {
 }
 
 const localStyle = StyleSheet.create({
-  list: {
-    textAlign: "center",
-  },
   fab: {
     position: "absolute",
     bottom: 25,
