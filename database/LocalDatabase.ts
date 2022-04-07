ca3ca4f0ed089bf84922155f318c50d23aea3740
SanglierF <lightrevan1996@gmail.com>
2022-04-07 13:19:40 +0200

    ## Changed: -added repositories as properties in local database -initialize repositories -App.tsx logic to reflect those changes -App.tsx removed unused functions, imports, etc.

--- database/LocalDatabase.ts
+++ database/LocalDatabase.ts
@@ -18,71 +18,104 @@ import OrderedProductRepository from "./repositories/OrderedProductRepository";
 
 export default class LocalDatabase {
   static dbInstance = null;
+  private _clientRepository: ClientRepository = null;
+  public get clientRepository(): ClientRepository {
+    return this._clientRepository;
+  }
+  private _productRepository: ProductRepository = null;
+  public get productRepository(): ProductRepository {
+    return this._productRepository;
+  }
+  private _visitRepository: VisitRepository = null;
+  public get visitRepository(): VisitRepository {
+    return this._visitRepository;
+  }
+  private _workdayRepository: WorkdayRepository = null;
+  public get workdayRepository(): WorkdayRepository {
+    return this._workdayRepository;
+  }
+  private _warehouseRepository: WarehouseRepository = null;
+  public get warehouseRepository(): WarehouseRepository {
+    return this._warehouseRepository;
+  }
+  private _orderRepository: OrderRepository = null;
+  public get orderRepository(): OrderRepository {
+    return this._orderRepository;
+  }
+  private _orderedProductRepository: OrderedProductRepository = null;
+  public get orderedProductRepository(): OrderedProductRepository {
+    return this._orderedProductRepository;
+  }
+
   dbConnection: Connection;
   constructor() {
-    DbConnection().then(
-      (value) => {
-        this.dbConnection = value;
-      },
-      () => {
-        this.dbConnection = null;
-        console.log("Baza popsuta");
-      }
-    );
+    this.dbConnection = null;
+  }
+  async initialize() {
+    try {
+      const dbConnection = await DbConnection();
+      this.dbConnection = dbConnection;
+      this._clientRepository = new ClientRepository(this.dbConnection);
+      this._productRepository = new ProductRepository(this.dbConnection);
+      this._visitRepository = new VisitRepository(this.dbConnection);
+      this._workdayRepository = new WorkdayRepository(this.dbConnection);
+      this._warehouseRepository = new WarehouseRepository(this.dbConnection);
+      this._orderRepository = new OrderRepository(this.dbConnection);
+      this._orderedProductRepository = new OrderedProductRepository(this.dbConnection);
+    } catch (error) {
+      console.log(error);
+      this.dbConnection = null;
+    }
+    if (!this.dbConnection) return console.log("Database broken");
+    return this.dbConnection;
   }
   async awaitDbConnection() {
-    while (this.dbConnection === undefined) {
+    while (!this.dbConnection) {
       await new Promise((resolve) => setTimeout(resolve, 100));
     }
     return true;
   }
   async populateDatabase() {
     let initalized = false;
-    // Repositories
-    const clientRepository = new ClientRepository(this.dbConnection);
-    const productRepository = new ProductRepository(this.dbConnection);
-    const visitRepository = new VisitRepository(this.dbConnection);
-    const workdayRepository = new WorkdayRepository(this.dbConnection);
-    const warehouseRepository = new WarehouseRepository(this.dbConnection);
     // products
-    const product1 = productRepository.create("Product1", 21.37);
-    const product2 = productRepository.create("Product2", 14.72);
-    const product3 = productRepository.create("Product3", 123.88);
+    const product1 = this.productRepository.create("Product1", 21.37);
+    const product2 = this.productRepository.create("Product2", 14.72);
+    const product3 = this.productRepository.create("Product3", 123.88);
     // warehouses
-    const warehouse1 = warehouseRepository.create({
+    const warehouse1 = this.warehouseRepository.create({
       nickname: "Warehouse1",
       nip: 123123123,
       regon: 123123123,
       name: "Full Warehouse1",
     });
-    const warehouse2 = warehouseRepository.create({
+    const warehouse2 = this.warehouseRepository.create({
       nickname: "Warehouse2",
       nip: 123123123,
       regon: 123123123,
       name: "Full Warehouse2",
     });
-    const warehouse3 = warehouseRepository.create({
+    const warehouse3 = this.warehouseRepository.create({
       nickname: "Warehouse3",
       nip: 123123123,
       regon: 123123123,
       name: "Full Warehouse3",
     });
     // clients
-    const client1 = clientRepository.create({ nickname: "Client1", name: "Full Client1" });
-    const client2 = clientRepository.create({ nickname: "Client2", name: "Full Client2" });
-    const client3 = clientRepository.create({ nickname: "Client3", name: "Full Client3" });
+    const client1 = this.clientRepository.create({ nickname: "Client1", name: "Full Client1" });
+    const client2 = this.clientRepository.create({ nickname: "Client2", name: "Full Client2" });
+    const client3 = this.clientRepository.create({ nickname: "Client3", name: "Full Client3" });
     // workdays
-    const workday1 = workdayRepository.create({ work_time_Begin: "2022-03-08T00:00:00.000Z" });
-    const workday2 = workdayRepository.create({ work_time_Begin: "2022-03-09T00:00:00.000Z" });
-    const workday3 = workdayRepository.create({ work_time_Begin: "2022-03-02T00:00:00.000Z" });
+    const workday1 = this.workdayRepository.create({ work_time_Begin: "2022-03-08T00:00:00.000Z" });
+    const workday2 = this.workdayRepository.create({ work_time_Begin: "2022-03-09T00:00:00.000Z" });
+    const workday3 = this.workdayRepository.create({ work_time_Begin: "2022-03-02T00:00:00.000Z" });
     // visits
     // save all
-    let found = await productRepository.getAll();
+    let found = await this.productRepository.getAll();
     if (found.length < 1) {
-      await productRepository.saveAll([product1, product2, product3]);
-      await warehouseRepository.saveAll([warehouse1, warehouse2, warehouse3]);
-      await clientRepository.saveAll([client1, client2, client3]);
-      await workdayRepository.saveAll([workday1, workday2, workday3]);
+      await this.productRepository.saveAll([product1, product2, product3]);
+      await this.warehouseRepository.saveAll([warehouse1, warehouse2, warehouse3]);
+      await this.clientRepository.saveAll([client1, client2, client3]);
+      await this.workdayRepository.saveAll([workday1, workday2, workday3]);
       initalized = true;
     }
     return initalized;
@@ -97,7 +130,7 @@ export default class LocalDatabase {
 async function DbConnection() {
   return await createConnection({
     name: "default",
-    database: "develop",
+    database: "develop2",
     driver: require("expo-sqlite"),
     entities: [Client, Product, Warehouse, Workday, Visit, Order, OrderedProduct],
     synchronize: true,
